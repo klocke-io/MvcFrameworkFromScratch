fd9295b23b1ee5c1b09d5f5b5a74f081d85ec2ea
Jan-Niklas Klocke <jan-niklasklocke@Jan-Niklass-MBP.fritz.box>
2020-11-19 19:15:52 +0100

    added missing autoloader and refactored it to work with namespaces.

--- Core/Autoloader.php
+++ Core/Autoloader.php
@@ -1,23 +1,11 @@
 <?php
 
-echo "Autoloader loaded" . PHP_EOL;
 spl_autoload_register(function ($class){
-    $path = "..\\" . $class . '.php';
+    //Get the parent directory
+    $root = dirname(__DIR__);
+    $path = $root .'\\'.$class . '.php';
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
-    echo "Core:" . PHP_EOL;
-    var_dump($path);
     if(file_exists($path)){
         require_once $path;
     }
 });
-
-spl_autoload_register(function ($class){
-    $path = '..\\' . $class . '.php';
-    $path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
-    echo "App:" . PHP_EOL;
-    var_dump($path);
-    if(file_exists($path)){
-        require_once $path;
-    }
-});
-
